aa6d54d9e3c3c9b726de283c1831903658a7bab8
mylhh <928436972@qq.com>
2019-12-02 22:33:09 +0800

    finish class Vector3

--- src/Main.java
+++ src/Main.java
@@ -13,22 +13,43 @@ import java.io.Writer;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void chapter1(){
         int width = 200;
         int height = 100;
         Image renderImage = Image.createImage(width,height,PPMFormat.PIXEL_ASCILL);
         int[] data = new int[height*width*3];
         for(int i = height-1;i >= 0 ;i--){
             for(int j = 0;j < width;j++){
-                float r = 1.0f* j / width;
-                float g = 1.0f* i / height;
-                float b = 0.2f;
+                double r = 1.0* j / width;
+                double g = 1.0* i / height;
+                double b = 0.2;
                 data[(height - 1 - i)*width*3+j*3] = (int)(255.999*r);
                 data[(height - 1 - i)*width*3+j*3+1] = (int)(255.999*g);
                 data[(height - 1 - i)*width*3+j*3+2] = (int)(255.999*b);
             }
         }
         renderImage.setData(data);
-        renderImage.save("./image/out.ppm");
+        renderImage.save("./image/chapter1.ppm");
+    }
+
+    public static void chapter2(){
+        int width = 200;
+        int height = 100;
+        Image renderImage = Image.createImage(width,height,PPMFormat.PIXEL_ASCILL);
+        int[] data = new int[height*width*3];
+        for(int i = height-1;i >= 0 ;i--){
+            for(int j = 0;j < width;j++){
+                Vector3 color = new Vector3(1.0* j / width,1.0* i / height,0.2);
+                data[(height - 1 - i)*width*3+j*3] = (int)(255.999*color.r());
+                data[(height - 1 - i)*width*3+j*3+1] = (int)(255.999*color.g());
+                data[(height - 1 - i)*width*3+j*3+2] = (int)(255.999*color.b());
+            }
+        }
+        renderImage.setData(data);
+        renderImage.save("./image/chapter2.ppm");
+    }
+    public static void main(String[] args) {
+        chapter1();
+        chapter2();
     }
 }
