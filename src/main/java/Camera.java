ea0eb648faccd633c606e386cbbe5c6e02ff8d46
mylhh <928436972@qq.com>
2019-12-08 00:14:30 +0800

    add positionable camera

--- src/main/java/Camera.java
+++ src/main/java/Camera.java
@@ -11,8 +11,23 @@ public class Camera {
         this.vertical = new Vector3(0.0,2.0,0.0);
         this.origin = Vector3.getZeroVector();
     }
+    // FOV(field of view)
+    public Camera(Vector3 lookfrom,Vector3 lookat,Vector3 viewUp,double verticalFOV,double aspect){
+        Vector3 u,v,w;
+        double theta = verticalFOV * Math.PI / 180; // 弧度
+        double halfHeight = Math.tan(theta / 2);
+        double halfWidth = aspect * halfHeight;
+        this.origin = lookfrom;
+        // 局部坐标
+        w = lookfrom.minus(lookat).makeUnitVector();
+        u = viewUp.cross(w);
+        v = w.cross(u);
+        this.lowerLeftPoint = origin.minus(u.scale(halfWidth)).minus(v.scale(halfHeight)).minus(w);
+        this.horizontal = u.scale(2 * halfWidth);
+        this.vertical = v .scale(2 * halfHeight);
+    }
 
     public Ray getRay(double u,double v){
-        return new Ray(origin,lowerLeftPoint.plus(horizontal.scale(u).plus(vertical.scale(v))));
+        return new Ray(origin,lowerLeftPoint.plus(horizontal.scale(u).plus(vertical.scale(v))).minus(origin));
     }
 }
